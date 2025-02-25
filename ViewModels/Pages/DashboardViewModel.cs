1ffa4bd064687b7b740e76cf95713b2c13d247f8
BYUNSUJUNG <susususu07@naver.com>
2025-02-25 17:54:51 +0900

    git init 2025-02-25

---
+++ ViewModels/Pages/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace UiDesktopAppTest.ViewModels.Pages
+{
+    public partial class DashboardViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? text = string.Empty;
+
+        [ObservableProperty]
+        private int _counter = 0;
+
+        [RelayCommand]
+        private void OnCounterIncrement()
+        {
+            //Counter++;
+            this.Text = "Clicked!!";
+        }
+    }
+}
---
+++ Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+using UiDesktopAppTest.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace UiDesktopAppTest.Views.Pages
+{
+    public partial class DashboardPage : INavigableView<DashboardViewModel>
+    {
+        public DashboardViewModel ViewModel { get; }
+
+        public DashboardPage(DashboardViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+            InitializeComponent();
+        }
+
+        private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName) {
+                case "Text":
+                    this.btnClickMe.Background = new SolidColorBrush(Colors.White);
+                    break;
+            }
+        }
+    }
+}
